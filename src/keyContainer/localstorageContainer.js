65a4a9df42104e5f490712910263eb5b1b33e03c
cjgarde <cjgarde@gmail.com>
2018-10-11 14:44:59 +0200

    Check if is node for websockets and node-localstorage

--- src/keyContainer/localstorageContainer.js
+++ src/keyContainer/localstorageContainer.js
@@ -3,9 +3,13 @@ const ethUtil = require('ethereumjs-util');
 const utils = require('../utils');
 const kcutils = require('./kcutils');
 
-if (typeof localStorage === "undefined" || localStorage === null) {
-  var LocalStorage = require('node-localstorage').LocalStorage;
-  localStorage = new LocalStorage('./scratch');
+// load node-localstorage if we are in a node environment,
+// if not, we are in a browser and already we have the local storage
+if (utils.isNodeEnv()){
+  if (typeof localStorage === "undefined" || localStorage === null) {
+    var LocalStorage = require('node-localstorage').LocalStorage;
+    localStorage = new LocalStorage('./scratch');
+  }
 }
 
 class LocalstorageContainer {
