038c5cb224cb49217f6b5c0f173a61dd5ca3dee9
arnaucube <arnaucube@gmail.com>
2018-11-22 10:41:11 +0100

    backup system version instead of timeout

--- src/backup.js
+++ src/backup.js
@@ -6,9 +6,10 @@ const kcUtils = require('./key-container/kc-utils');
 class Backup {
   constructor(url) {
     this.url = url; // backup server url
+    this.version = 0; // current last used version
   }
 
-  getPoWDifficulty () {
+  getPoWDifficulty() {
     return axios.get(`${this.url}/`, {});
   }
 
@@ -22,7 +23,7 @@ class Backup {
    * @param  {String} relayAddr
    * @returns {Object}
    */
-  backupData (kc, idaddr, ksign, proofOfKSign, type, data, difficulty, relayAddr) {
+  backupData(kc, idaddr, ksign, proofOfKSign, type, data, difficulty, relayAddr) {
     // TODO relayAddr will be setted in a global config, not passed in this function as parameter
     /*
       types:
@@ -33,6 +34,7 @@ class Backup {
         - logs (history)
         - ...
     */
+    this.version++;
     const dataEncrypted = kcUtils.encrypt(kc.encryptionKey, data);
     let dataPacket = {
       idaddrhex: idaddr,
@@ -42,7 +44,7 @@ class Backup {
       ksign: ksign,
       proofofksignhex: proofOfKSign,
       relayaddr: relayAddr,
-      timestamp: 0, // will be setted by the backend
+      version: this.version, // version from the last stored value
       nonce: 0 // will be setted in next step of PoW
     };
     // PoW
@@ -56,17 +58,17 @@ class Backup {
    * @param  {String} idaddr - hex representation of idaddr
    * @returns {Object}
    */
-  recoverData (idaddr) {
+  recoverData(idaddr) {
     return axios.post(`${this.url}/${idaddr}/recover`, {});
   }
 
   /**
    * @param  {String} idaddr - hex representation of idaddr
-   * @param {Number} timestamp - unixtime
+   * @param {Number} version - unixtime
    * @returns {Object}
    */
-  recoverDataByTimestamp (idaddr, timestamp) {
-    return axios.post(`${this.url}/${idaddr}/recover/timestamp/${timestamp}`, {});
+  recoverDataSinceVersion(idaddr, version) {
+    return axios.post(`${this.url}/${idaddr}/recover/version/${version}`, {});
   }
 
   /**
@@ -74,7 +76,7 @@ class Backup {
    * @param {String} type - typeof the data
    * @returns {Object}
    */
-  recoverDataByType (idaddr, type) {
+  recoverDataByType(idaddr, type) {
     return axios.post(`${this.url}/${idaddr}/recover/type/${type}`, {});
   }
 }
