fc8a9db4ca47ebd58903f5b1f3c0480c2bb71c05
Eduard S <eduard@iden3.io>
2019-01-30 15:44:06 +0100

    Add debug axios get and post

--- src/http/relay.js
+++ src/http/relay.js
@@ -1,6 +1,35 @@
 const axios = require('axios');
 const utils = require('../utils');
 
+async function axios_get_debug(url) {
+  console.log("### GET " + url);
+  const res = await axios.get(url).then((_res) => {
+    console.log("Returns:\n```js");
+    console.log(JSON.stringify(_res.data, null, 2));
+    console.log("```");
+    return _res;
+  }).catch((_err) => {
+    console.log("Error:" + _err);
+  });
+  return Promise.resolve(res);
+}
+
+async function axios_post_debug(url, data) {
+  console.log("### POST " + url);
+  console.log("Input:\n```js");
+  console.log(JSON.stringify(data, null, 2));
+  console.log("```");
+  const res = await axios.post(url, data).then((_res) => {
+    console.log("Returns:\n```js");
+    console.log(JSON.stringify(_res.data, null, 2));
+    console.log("```");
+    return _res;
+  }).catch((_err) => {
+    console.log("Error:" + _err);
+  });
+  return Promise.resolve(res);
+}
+
 /**
  * Class representing a the Relay
  * It contains all the relay API calls
