22c9b529ec19758abd06b585303b03c914053ee8
Eduard S <eduard@iden3.io>
2019-03-05 12:15:55 +0100

    Add axios config param in http debug

--- src/http/http-debug.js
+++ src/http/http-debug.js
@@ -1,9 +1,9 @@
 const axios = require('axios');
 
 /* eslint no-console: "off" */
-export async function axiosGetDebug(url) {
+export async function axiosGetDebug(url, config) {
   console.log(`### GET ${url}`);
-  const res = await axios.get(url).then((_res) => {
+  const res = await axios.get(url, config).then((_res) => {
     console.log('Returns:\n```js');
     console.log(JSON.stringify(_res.data, null, 2));
     console.log('```');
@@ -14,12 +14,12 @@ export async function axiosGetDebug(url) {
   return Promise.resolve(res);
 }
 
-export async function axiosPostDebug(url, data) {
+export async function axiosPostDebug(url, data, config) {
   console.log(`### POST ${url}`);
   console.log('Input:\n```js');
   console.log(JSON.stringify(data, null, 2));
   console.log('```');
-  const res = await axios.post(url, data).then((_res) => {
+  const res = await axios.post(url, data, config).then((_res) => {
     console.log('Returns:\n```js');
     console.log(JSON.stringify(_res.data, null, 2));
     console.log('```');
