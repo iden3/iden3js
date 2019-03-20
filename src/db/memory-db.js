ebeb35e1d4f6aef5c61701d222658e5bcc5f678b
krlosMata <krlosmata@gmail.com>
2019-03-20 13:45:14 +0100

    add database memory allocation

---
+++ src/db/memory-db.js
@@ -0,0 +1,49 @@
+const CONSTANTS = require('../constants');
+
+/**
+ * Database interface for memory allocation
+ */
+class MemorydB {
+  /**
+   * @param {Bool} prefix - Database prefix 'i3db-' added to key values
+   */
+  constructor(prefix = true) {
+    this.prefix = prefix ? CONSTANTS.DBPREFIX : '';
+    this.db = new Map();
+  }
+
+  /**
+   * Method to store [key - value] on database
+   * @param {String} key
+   * @param {String} value
+   */
+  insert(key, value) {
+    this.db.set(this.prefix + key, value);
+  }
+
+  /**
+   * Method to retrieve a value given a key
+   * @param {String} key
+   * @returns {String}
+   */
+  get(key) {
+    return this.db.get(this.prefix + key);
+  }
+
+  /**
+   * Method to delete a value given a key
+   * @param {String} key
+   */
+  delete(key) {
+    this.db.delete(this.prefix + key);
+  }
+
+  /**
+   * Method to delete all the [key - value] items
+   */
+  deleteAll() {
+    this.db.clear();
+  }
+}
+
+module.exports = MemorydB;
